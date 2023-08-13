c881df7ea6a2bdd3fa2f0e76576a32bf49bbe61e
SANK123RAJ <sankalpraj2016@gmail.com>
2023-08-13 18:41:30 +0530

    Trying to Deploy

--- project3/settings.py
+++ project3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '05$4$3aew(8ywondz$g!k4m779pbvn9)euj0zp7-ae*x@4pxr+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -121,7 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-import os
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
